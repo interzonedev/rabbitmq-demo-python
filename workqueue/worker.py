5049478024d895435c8b7cd07349232a122d4757
Mark Markarian <mark@interzonedev.com>
2012-08-01 23:47:00 -0700

    Added workqueue examples.

---
+++ workqueue/worker.py
@@ -0,0 +1,22 @@
+import pika
+import time
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+        host='localhost'))
+channel = connection.channel()
+
+channel.queue_declare(queue='task_queue', durable=True)
+print ' [*] Waiting for messages. To exit press CTRL+C'
+
+def callback(ch, method, properties, body):
+    print " [x] Received %r" % (body,)
+    time.sleep( body.count('.') )
+    print " [x] Done"
+    ch.basic_ack(delivery_tag = method.delivery_tag)
+
+channel.basic_qos(prefetch_count=1)
+consumer_tag = channel.basic_consume(callback,
+                      queue='task_queue')
+print " [*] consumer_tag %r" % (consumer_tag,)
+
+channel.start_consuming()

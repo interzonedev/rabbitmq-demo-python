01fee4e1baa98141972d4ff81ebb26af8e79a2c6
Mark Markarian <mark@interzonedev.com>
2012-08-05 10:14:43 -0700

    Added topic examples.

--- topic/receive_logs_topic.py
+++ topic/receive_logs_topic.py
@@ -5,23 +5,23 @@ connection = pika.BlockingConnection(pika.ConnectionParameters(
         host='localhost'))
 channel = connection.channel()
 
-channel.exchange_declare(exchange='direct_logs',
-                         type='direct')
+channel.exchange_declare(exchange='topic_logs',
+                         type='topic')
 
 result = channel.queue_declare(exclusive=True)
 print " [*] result = %r" % (result,)
 queue_name = result.method.queue
 print " [*] queue_name = %r" % (queue_name,)
 
-severities = sys.argv[1:]
-if not severities:
-    print >> sys.stderr, "Usage: %s [info] [warning] [error]" % (sys.argv[0],)
+binding_keys = sys.argv[1:]
+if not binding_keys:
+    print >> sys.stderr, "Usage: %s [binding_key]..." % (sys.argv[0],)
     sys.exit(1)
 
-for severity in severities:
-    bind_ok = channel.queue_bind(exchange='direct_logs',
+for binding_key in binding_keys:
+    bind_ok = channel.queue_bind(exchange='topic_logs',
                        queue=queue_name,
-                       routing_key=severity)
+                       routing_key=binding_key)
     print " [*] bind_ok = %r" % (bind_ok,)
 
 print ' [*] Waiting for logs. To exit press CTRL+C'

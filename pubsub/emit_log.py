fcebb661ce078f16a52a2dcb77c840b9225aaf6b
Mark Markarian <mark@interzonedev.com>
2012-08-09 00:32:48 -0700

    Made the pubsub examples object oriented.

--- pubsub/emit_log.py
+++ pubsub/emit_log.py
@@ -1,18 +1,60 @@
-import pika
-import sys
+#!/usr/bin/env python
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(
-        host='localhost'))
-channel = connection.channel()
+import demo.utils
 
-declare_ok = channel.exchange_declare(exchange='logs',
-                         type='fanout')
-print " [*] declare_ok = %r" % (declare_ok,)
+class EmitLog(object):
+    EXCHANGE_NAME = 'logs'
 
-message = ' '.join(sys.argv[1:]) or "info: Hello World!"
-channel.basic_publish(exchange='logs',
-                      routing_key='',
-                      body=message)
-print " [x] Sent %r" % (message,)
+    def __init__(self):
+        print "EmitLog: Constructing instance"
+        self.initialized = False
+        self.connection = None
 
-connection.close()
+    def __del__(self):
+        print "EmitLog: Deleting instance"
+        self.destroy()
+
+    def initialize(self):
+        print "EmitLog: initialize"
+
+        self.initialized = True
+        self.connection = demo.utils.get_connection()
+
+        self.channel = self.connection.channel()
+
+        self.channel.exchange_declare(exchange=self.EXCHANGE_NAME, type='fanout')
+
+    def destroy(self):
+        print "EmitLog: destroy"
+        if (self.connection is not None) and (not self.connection.closed):
+            self.connection.close()
+
+    def emit(self, message):
+        print "EmitLog: send"
+
+        if not self.initialized:
+            self.initialize()
+
+        self.channel.basic_publish(exchange=self.EXCHANGE_NAME, routing_key='', body=message)
+        
+def main(args):
+    emitLog = None
+    try:
+        message = ' '.join(args) or "info: Hello World!"
+        emitLog = EmitLog()
+        print "Emitting %r" % (message,)
+        emitLog.emit(message)
+        return 0
+    except KeyboardInterrupt:
+        print "Exiting"
+        return 0
+    except Exception, e:
+        print "Exception"
+        print str(e)
+        return 1
+    finally:
+        del emitLog
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv[1:]))
--- pubsub/receive_logs.py
+++ pubsub/receive_logs.py
@@ -1,29 +1,68 @@
-import pika
+#!/usr/bin/env python
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(
-        host='localhost'))
-channel = connection.channel()
+import demo.utils
 
-declare_ok = channel.exchange_declare(exchange='logs',
-                         type='fanout')
-print " [*] declare_ok = %r" % (declare_ok,)
+class ReceiveLog(object):
+    EXCHANGE_NAME = 'logs'
 
-result = channel.queue_declare(exclusive=True)
-queue_name = result.method.queue
-print " [*] queue_name = %r" % (queue_name,)
+    def __init__(self):
+        print "ReceiveLog: Constructing instance"
+        self.connection = None
 
-bind_ok = channel.queue_bind(exchange='logs',
-                   queue=queue_name)
-print " [*] bind_ok = %r" % (bind_ok,)
+    def __del__(self):
+        print "ReceiveLog: Deleting instance"
+        self.destroy()
 
-def callback(ch, method, properties, body):
-    print " [x] %r" % (body,)
+    def destroy(self):
+        print "ReceiveLog: destroy"
+        if (self.connection is not None) and (not self.connection.closed):
+            self.connection.close()
 
-consumer_tag = channel.basic_consume(callback,
-                      queue=queue_name,
-                      no_ack=True)
-print " [*] consumer_tag = %r" % (consumer_tag,)
+    def listen(self):
+        print "ReceiveLog: listen"
 
-print ' [*] Waiting for logs. To exit press CTRL+C'
+        self.connection = demo.utils.get_connection()
 
-channel.start_consuming()
+        self.channel = self.connection.channel()
+        declare_ok = self.channel.exchange_declare(exchange=self.EXCHANGE_NAME, type='fanout')
+        print "ReceiveLog: declare_ok = %r" % (declare_ok,)
+
+        result = self.channel.queue_declare(exclusive=True)
+        queue_name = result.method.queue
+        print "ReceiveLog: queue_name = %r" % (queue_name,)
+
+        bind_ok = self.channel.queue_bind(exchange=self.EXCHANGE_NAME, queue=queue_name)
+        print "ReceiveLog: bind_ok = %r" % (bind_ok,)
+
+        consumer_tag = self.channel.basic_consume(self.received_message, queue=queue_name, no_ack=True)
+        print "ReceiveLog: consumer_tag = %r" % (consumer_tag,)
+
+        print "ReceiveLog: Waiting for messages. To exit press CTRL+C"
+
+        try:
+            self.channel.start_consuming()
+        finally:
+            self.destroy()
+
+    def received_message(self, ch, method, properties, body):
+        print "ReceiveLog: Received %r" % (body,)
+
+def main(args):
+    receiveLog = None
+    try:
+        receiveLog = ReceiveLog()
+        receiveLog.listen()
+        return 0
+    except KeyboardInterrupt:
+        print "Exiting"
+        return 0
+    except Exception, e:
+        print "Exception"
+        print str(e)
+        return 1
+    finally:
+        del receiveLog
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv[1:]))

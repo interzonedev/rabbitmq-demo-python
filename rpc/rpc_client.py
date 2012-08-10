149a41570856740d6be2cb2fcd34a70e0d6b03b6
Mark Markarian <mark@interzonedev.com>
2012-08-10 09:15:22 -0700

    Made RPC examples object oriented.

--- rpc/rpc_client.py
+++ rpc/rpc_client.py
@@ -1,37 +1,58 @@
+#!/usr/bin/env python
+
 import pika
 import uuid
-import sys
 
-class FibonacciRpcClient(object):
+import demo.utils
+
+class RpcClient(object):
+    ROUTING_KEY = 'rpc_queue'
+
     def __init__(self):
-        self.connection = pika.BlockingConnection(pika.ConnectionParameters(
-                host='localhost'))
+        print "RpcClient: Constructing instance"
+        self.initialized = False
+        self.connection = None
+
+    def __del__(self):
+        print "RpcClient: Deleting instance"
+        self.destroy()
+
+    def initialize(self):
+        print "RpcClient: initialize"
+
+        self.initialized = True
+        self.connection = demo.utils.get_connection()
 
         self.channel = self.connection.channel()
 
         result = self.channel.queue_declare(exclusive=True)
-        print " [*] result = %r" % (result,)
+        print "RpcClient: result = %r" % (result,)
         self.callback_queue = result.method.queue
-        print " [*] self.callback_queue = %r" % (self.callback_queue,)
+        print "RpcClient: self.callback_queue = %r" % (self.callback_queue,)
 
-        self.channel.basic_consume(self.on_response, no_ack=True,
-                                   queue=self.callback_queue)
+        self.channel.basic_consume(self.on_response, no_ack=True, queue=self.callback_queue)
 
-    def on_response(self, ch, method, props, body):
-        if self.corr_id == props.correlation_id:
-            self.response = body
+    def destroy(self):
+        print "RpcClient: destroy"
+        if (self.connection is not None) and (not self.connection.closed):
+            self.connection.close()
 
     def call(self, n):
+        print "RpcClient: call"
+
+        if not self.initialized:
+            self.initialize()
+
         self.response = None
 
         self.corr_id = str(uuid.uuid4())
-        print " [*] call:corr_id = %r" % (self.corr_id,)
+        print "RpcClient: corr_id = %r" % (self.corr_id,)
 
         self.channel.basic_publish(exchange='',
-                                   routing_key='rpc_queue',
+                                   routing_key=self.ROUTING_KEY,
                                    properties=pika.BasicProperties(
-                                         reply_to = self.callback_queue,
-                                         correlation_id = self.corr_id,
+                                             reply_to = self.callback_queue,
+                                             correlation_id = self.corr_id,
                                          ),
                                    body=str(n))
 
@@ -40,9 +61,33 @@ class FibonacciRpcClient(object):
 
         return int(self.response)
 
-fibonacci_rpc = FibonacciRpcClient()
+    def on_response(self, ch, method, props, body):
+        print "RpcClient: on_response"
 
-fib_start = sys.argv[1] if len(sys.argv) > 1 else 30
-print " [x] Requesting fib(%r)" % (fib_start,)
-response = fibonacci_rpc.call(fib_start)
-print " [.] Got %r" % (response,)
+        if self.corr_id == props.correlation_id:
+            self.response = body
+
+def main(args):
+    rpc_client = None
+    try:
+        fib_start = args[0] if len(args) > 0 else 30
+        print "Requesting fib(%r)" % (fib_start,)
+        
+        rpc_client = RpcClient()
+        response = rpc_client.call(fib_start)
+        print "Got %r" % (response,)
+
+        return 0
+    except KeyboardInterrupt:
+        print "Exiting"
+        return 0
+    except Exception, e:
+        print "Exception"
+        print str(e)
+        return 1
+    finally:
+        del rpc_client
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv[1:]))
--- rpc/rpc_server.py
+++ rpc/rpc_server.py
@@ -1,39 +1,82 @@
+#!/usr/bin/env python
+
 import pika
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(
-        host='localhost'))
+import demo.utils
 
-channel = connection.channel()
+class RpcServer(object):
+    QUEUE_NAME = 'rpc_queue'
 
-result = channel.queue_declare(queue='rpc_queue')
-print " [*] result = %r" % (result,)
-queue_name = result.method.queue
-print " [*] queue_name = %r" % (queue_name,)
+    def __init__(self):
+        print "RpcServer: Constructing instance"
+        self.connection = None
 
-def fib(n):
-    if n == 0:
+    def __del__(self):
+        print "RpcServer: Deleting instance"
+        self.destroy()
+
+    def destroy(self):
+        print "RpcServer: destroy"
+        if (self.connection is not None) and (not self.connection.closed):
+            self.connection.close()
+
+    def listen(self):
+        print "RpcServer: listen"
+
+        self.connection = demo.utils.get_connection()
+
+        channel = self.connection.channel()
+
+        result = channel.queue_declare(queue=self.QUEUE_NAME)
+        print "RpcServer: result = %r" % (result,)
+
+        channel.basic_qos(prefetch_count=1)
+        channel.basic_consume(self.on_request, queue=self.QUEUE_NAME)
+
+        print "RpcServer: Waiting for messages. To exit press CTRL+C"
+
+        try:
+            channel.start_consuming()
+        finally:
+            self.destroy()
+
+    def fib(self, n):
+        if n == 0:
+            return 0
+        elif n == 1:
+            return 1
+        else:
+            return self.fib(n-1) + self.fib(n-2)
+
+    def on_request(self, ch, method, props, body):
+        n = int(body)
+
+        print "RpcServer: fib(%s)"  % (n,)
+        response = self.fib(n)
+        print "RpcServer: response = %r" % (response,)
+
+        ch.basic_publish(exchange='',
+                         routing_key=props.reply_to,
+                         properties=pika.BasicProperties(correlation_id = props.correlation_id),
+                         body=str(response))
+        ch.basic_ack(delivery_tag = method.delivery_tag)
+
+def main(args):
+    rpc_server = None
+    try:
+        rpc_server = RpcServer()
+        rpc_server.listen()
         return 0
-    elif n == 1:
+    except KeyboardInterrupt:
+        print "Exiting"
+        return 0
+    except Exception, e:
+        print "Exception"
+        print str(e)
         return 1
-    else:
-        return fib(n-1) + fib(n-2)
+    finally:
+        del rpc_server
 
-def on_request(ch, method, props, body):
-    n = int(body)
-
-    print " [.] fib(%s)"  % (n,)
-    response = fib(n)
-    print " [*] response = %r" % (response,)
-
-    ch.basic_publish(exchange='',
-                     routing_key=props.reply_to,
-                     properties=pika.BasicProperties(correlation_id = \
-                                                         props.correlation_id),
-                     body=str(response))
-    ch.basic_ack(delivery_tag = method.delivery_tag)
-
-channel.basic_qos(prefetch_count=1)
-channel.basic_consume(on_request, queue='rpc_queue')
-
-print " [x] Awaiting RPC requests"
-channel.start_consuming()
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv[1:]))

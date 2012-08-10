f9cd480ac95e34da8fc66adb3c3774971fb67c59
Mark Markarian <mark@interzonedev.com>
2012-08-10 00:01:07 -0700

    Made topic exampled object oriented.

--- topic/emit_log_topic.py
+++ topic/emit_log_topic.py
@@ -1,19 +1,62 @@
-import pika
-import sys
+#!/usr/bin/env python
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(
-        host='localhost'))
-channel = connection.channel()
+import demo.utils
 
-channel.exchange_declare(exchange='topic_logs',
-                         type='topic')
+class EmitLogTopic(object):
+    EXCHANGE_NAME = 'topic_logs'
 
-routing_key = sys.argv[1] if len(sys.argv) > 1 else 'anonymous.info'
-message = ' '.join(sys.argv[2:]) or 'Hello World!'
+    def __init__(self):
+        print "EmitLogTopic: Constructing instance"
+        self.initialized = False
+        self.connection = None
 
-channel.basic_publish(exchange='topic_logs',
-                      routing_key=routing_key,
-                      body=message)
-print " [x] Sent %r:%r" % (routing_key, message)
+    def __del__(self):
+        print "EmitLogTopic: Deleting instance"
+        self.destroy()
 
-connection.close()
+    def initialize(self):
+        print "EmitLogTopic: initialize"
+
+        self.initialized = True
+        self.connection = demo.utils.get_connection()
+
+        self.channel = self.connection.channel()
+        self.channel.exchange_declare(exchange=self.EXCHANGE_NAME, type='topic')
+
+    def destroy(self):
+        print "EmitLogTopic: destroy"
+        if (self.connection is not None) and (not self.connection.closed):
+            self.connection.close()
+
+    def send(self, routing_key, message):
+        print "EmitLogTopic: send"
+
+        if not self.initialized:
+            self.initialize()
+
+        self.channel.basic_publish(exchange=self.EXCHANGE_NAME, routing_key=routing_key, body=message)
+        print "EmitLogTopic: Sent %r:%r" % (routing_key, message)
+
+def main(args):
+    emit_log_topic = None
+    try:
+        routing_key = args[0] if len(args) > 0 else 'anonymous.info'
+        message = ' '.join(args[1:]) or 'Hello World!'
+
+        emit_log_topic = EmitLogTopic()
+        print "Sending %r %r" % (routing_key, message,)
+        emit_log_topic.send(routing_key, message)
+        return 0
+    except KeyboardInterrupt:
+        print "Exiting"
+        return 0
+    except Exception, e:
+        print "Exception"
+        print str(e)
+        return 1
+    finally:
+        del emit_log_topic
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv[1:]))
--- topic/receive_logs_topic.py
+++ topic/receive_logs_topic.py
@@ -1,36 +1,74 @@
-import pika
-import sys
+#!/usr/bin/env python
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(
-        host='localhost'))
-channel = connection.channel()
+import demo.utils
 
-channel.exchange_declare(exchange='topic_logs',
-                         type='topic')
+class ReceiveLogsTopic(object):
+    EXCHANGE_NAME = 'topic_logs'
 
-result = channel.queue_declare(exclusive=True)
-print " [*] result = %r" % (result,)
-queue_name = result.method.queue
-print " [*] queue_name = %r" % (queue_name,)
+    def __init__(self, binding_keys):
+        print "ReceiveLogsTopic: Constructing instance"
+        self.connection = None
+        self.binding_keys = binding_keys
 
-binding_keys = sys.argv[1:]
-if not binding_keys:
-    print >> sys.stderr, "Usage: %s [binding_key]..." % (sys.argv[0],)
-    sys.exit(1)
+    def __del__(self):
+        print "ReceiveLogsTopic: Deleting instance"
+        self.destroy()
 
-for binding_key in binding_keys:
-    bind_ok = channel.queue_bind(exchange='topic_logs',
-                       queue=queue_name,
-                       routing_key=binding_key)
-    print " [*] bind_ok = %r" % (bind_ok,)
+    def destroy(self):
+        print "ReceiveLogsTopic: destroy"
+        if (self.connection is not None) and (not self.connection.closed):
+            self.connection.close()
 
-print ' [*] Waiting for logs. To exit press CTRL+C'
+    def listen(self):
+        print "ReceiveLogsTopic: listen"
 
-def callback(ch, method, properties, body):
-    print " [x] %r:%r" % (method.routing_key, body,)
+        self.connection = demo.utils.get_connection()
+        channel = self.connection.channel()
 
-channel.basic_consume(callback,
-                      queue=queue_name,
-                      no_ack=True)
+        channel.exchange_declare(exchange=self.EXCHANGE_NAME, type='topic')
 
-channel.start_consuming()
+        result = channel.queue_declare(exclusive=True)
+        print "ReceiveLogsTopic: result = %r" % (result,)
+        queue_name = result.method.queue
+        print "ReceiveLogsTopic: queue_name = %r" % (queue_name,)
+
+        for binding_key in self.binding_keys:
+            bind_ok = channel.queue_bind(exchange=self.EXCHANGE_NAME, queue=queue_name, routing_key=binding_key)
+            print "ReceiveLogsTopic: bind_ok = %r" % (bind_ok,)
+
+        channel.basic_consume(self.received_message, queue=queue_name, no_ack=True)
+
+        print "ReceiveLogsTopic: Waiting for messages. To exit press CTRL+C"
+
+        try:
+            channel.start_consuming()
+        finally:
+            self.destroy()
+
+    def received_message(self, ch, method, properties, body):
+        print "ReceiveLogsTopic: Received %r" % (body,)
+
+def main(args):
+    receive_logs_topic = None
+    try:
+        binding_keys = args[0:]
+        if not binding_keys:
+            print "Usage: %s [binding_key]..." % (args[0],)
+            return 2
+
+        receive_logs_topic = ReceiveLogsTopic(binding_keys)
+        receive_logs_topic.listen()
+        return 0
+    except KeyboardInterrupt:
+        print "Exiting"
+        return 0
+    except Exception, e:
+        print "Exception"
+        print str(e)
+        return 1
+    finally:
+        del receive_logs_topic
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv[1:]))
